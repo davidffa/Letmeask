247e6833c8b649424dbf1bc8464973111298aea4
David Amorim <davidfilipeamorim@gmail.com>
2021-08-04 16:18:44 +0100

    Add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './services/firebase'
+import './services/firebase';
+
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
